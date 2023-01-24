15abbd104f4dc33c8214b3420492ba99331f74a6
ldm <lucas.meier@csem.ch>
2023-01-24 14:49:40 +0100

    add pool

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-central-1_Pis6nUVO8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '44730r6ltu9982lrfu2nmbsqn6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
